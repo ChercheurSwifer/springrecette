a954caa4064dd3344908cd008349dd210ac6bd56
Alexis Larochette <58219897+ChercheurSwifer@users.noreply.github.com>
2022-01-05 14:01:13 +0100

    Delete src/main/java/fr/alexis/springbootrecette directory

--- src/main/java/fr/alexis/springbootrecette/repositories/IngredientsRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.alexis.springbootrecette.repositories;
-
-import fr.alexis.springbootrecette.models.Ingredients;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface IngredientsRepository extends MongoRepository<Ingredients, String>
-{
-}
--- src/main/java/fr/alexis/springbootrecette/repositories/RecetteRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.alexis.springbootrecette.repositories;
-
-import fr.alexis.springbootrecette.models.Recette;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RecetteRepository extends MongoRepository<Recette, String> {
-}
